40946d895aab40360a9a1c82cbeb6784b8ae2691
abhijitraijada <36490566+abhijitraijada@users.noreply.github.com>
2025-08-28 22:57:18 +0530

    Updated the database configuration

--- app/utils/database.py
+++ app/utils/database.py
@@ -7,7 +7,7 @@ from app.utils.logger import logger
 settings = get_settings()
 
 # Create database engine
-engine = create_engine(settings.VAANI_DATABASE_URL, pool_pre_ping=True, pool_recycle=1800, pool_size=10)
+engine = create_engine(settings.VAANI_DATABASE_URL, pool_pre_ping=True, pool_recycle=300, pool_size=5, max_overflow=2)
 
 # Create SessionLocal class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
